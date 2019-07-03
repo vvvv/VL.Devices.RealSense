bf1d580e1aa68f97a893e6ed5257b9b9d0232e2b
untone <anton@vvvv.org>
2019-07-03 22:09:51 +0200

    WIP. Introduced Enums for the CameraDevice, Color and Depth sensor Resolutions and Framerates. At the moment populating enums triggers recompile that leads to exception.

--- src/Enums.cs
+++ src/Enums.cs
@@ -8,31 +8,6 @@ using VL.Core;
 
 namespace VL.Devices.RealSense
 {
-    //public class RealSenseDevice
-    //{
-    //    public string Update(RealSenseDevice enumInput)
-    //    {
-    //        if (enumInput.IsValid())
-    //            return enumInput.Definition.Entries[enumInput.SelectedIndex()];
-    //        else
-    //            return "No valid entry selected";
-    //    }
-
-    //    public RealSenseDeviceDefinition GetDefinition()
-    //    {
-    //        return RealSenseDevice.Instance;
-    //    }
-
-    //    public void AddEnumEntry(string entry)
-    //    {
-    //        RealSenseDeviceDefinition.Instance.AddEntry(entry, null);
-    //    }
-
-    //    public void RemoveEnumEntry(string entry)
-    //    {
-    //        RealSenseDeviceDefinition.Instance.RemoveEntry(entry);
-    //    }
-    //}
 
     public class RealSenseDeviceDefinition : ManualDynamicEnumDefinitionBase<RealSenseDeviceDefinition>
     {
@@ -51,6 +26,98 @@ namespace VL.Devices.RealSense
 
         //this method needs to be imported in VL to set the default
         public static RealSenseDevice CreateDefault()
+        {
+            //use method of base class if nothing special required
+            return CreateDefaultBase();
+        }
+    }
+
+    public class DepthResolutionDefinition : ManualDynamicEnumDefinitionBase<DepthResolutionDefinition>
+    {
+
+        //add this to get a node that can access the Instance from everywhere
+        public static DepthResolutionDefinition Instance => ManualDynamicEnumDefinitionBase<DepthResolutionDefinition>.Instance;
+        override protected bool AutoSortAlphabetically => false;
+    }
+
+    [Serializable]
+    public class DepthResolution : DynamicEnumBase<DepthResolution, DepthResolutionDefinition>
+    {
+        public DepthResolution(string value) : base(value)
+        {
+        }
+
+        //this method needs to be imported in VL to set the default
+        public static DepthResolution CreateDefault()
+        {
+            //use method of base class if nothing special required
+            return CreateDefaultBase();
+        }
+    }
+
+    public class ColorResolutionDefinition : ManualDynamicEnumDefinitionBase<ColorResolutionDefinition>
+    {
+
+        //add this to get a node that can access the Instance from everywhere
+        public static ColorResolutionDefinition Instance => ManualDynamicEnumDefinitionBase<ColorResolutionDefinition>.Instance;
+        override protected bool AutoSortAlphabetically => false;
+    }
+
+    [Serializable]
+    public class ColorResolution : DynamicEnumBase<ColorResolution, ColorResolutionDefinition>
+    {
+        public ColorResolution (string value) : base(value)
+        {
+        }
+
+        //this method needs to be imported in VL to set the default
+        public static ColorResolution CreateDefault()
+        {
+            //use method of base class if nothing special required
+            return CreateDefaultBase();
+        }
+    }
+
+    public class ColorFramerateDefinition : ManualDynamicEnumDefinitionBase<ColorFramerateDefinition>
+    {
+
+        //add this to get a node that can access the Instance from everywhere
+        public static ColorFramerateDefinition Instance => ManualDynamicEnumDefinitionBase<ColorFramerateDefinition>.Instance;
+        override protected bool AutoSortAlphabetically => false;
+    }
+
+    [Serializable]
+    public class ColorFramerate : DynamicEnumBase<ColorFramerate, ColorFramerateDefinition>
+    {
+        public ColorFramerate (string value) : base(value)
+        {
+        }
+
+        //this method needs to be imported in VL to set the default
+        public static ColorFramerate CreateDefault()
+        {
+            //use method of base class if nothing special required
+            return CreateDefaultBase();
+        }
+    }
+
+    public class DepthFramerateDefinition : ManualDynamicEnumDefinitionBase<DepthFramerateDefinition>
+    {
+
+        //add this to get a node that can access the Instance from everywhere
+        public static DepthFramerateDefinition Instance => ManualDynamicEnumDefinitionBase<DepthFramerateDefinition>.Instance;
+        override protected bool AutoSortAlphabetically => false;
+    }
+
+    [Serializable]
+    public class DepthFramerate : DynamicEnumBase<DepthFramerate, DepthFramerateDefinition>
+    {
+        public DepthFramerate (string value) : base(value)
+        {
+        }
+
+        //this method needs to be imported in VL to set the default
+        public static DepthFramerate CreateDefault()
         {
             //use method of base class if nothing special required
             return CreateDefaultBase();
