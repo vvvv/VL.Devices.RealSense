7e3b8405889eba8fe6854842515d9af5cad241d2
untone <anton@vvvv.org>
2019-07-10 21:29:10 +0200

    Reworked with Observables. /build

---
+++ src/ObservableHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VL.Devices.RealSense
+{
+    public static class ObservableHelper
+    {
+        public static IObservable<T> Forever<T>(Func<T> producer)
+        {
+            return Observable.Create<T>(async (observer, token) =>
+            {
+                await Task.Run(() =>
+                {
+                    while (!token.IsCancellationRequested)
+                    {
+                        var value = producer();
+                        observer.OnNext(value);
+                    }
+                });
+            });
+        }
+    }
+}
